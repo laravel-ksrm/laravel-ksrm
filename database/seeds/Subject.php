73ca8b8f88ac08b3c4e1aabc9f353d01b499e4d1
laravel-ksrm <lhmr78846@gmail.com>
2021-02-09 17:00:15 +0530

    updates

--- database/seeds/Subject.php
+++ database/seeds/Subject.php
@@ -11,7 +11,51 @@ class Subject extends Seeder
      */
     public function run()
     {
-        factory(App\Subject::class,5)->create();
+        DB::table('subjects')->delete();
+    $statement = "ALTER TABLE users AUTO_INCREMENT = 1;";
+    DB::unprepared($statement);
+
+    $csv = dirname(__FILE__) . '/data/' . 'ssub.csv';
+    $file_handle = fopen($csv, "r");
+
+    echo PHP_EOL;
+    echo '------------------------------------------ open file ------------------------------------------';
+    echo PHP_EOL;
+
+    while (!feof($file_handle)) {
+
+        $line = fgetcsv($file_handle);
+        if (empty($line)) {
+            continue; // skip blank lines
+        }
+
+    //    Pro-id,sn,name,sy,ey,ts,tc,pcgpa
+    
+            
+            
+            $insert = array();
+            $insert['code'] = $line[0];
+            $insert['short_name'] = $line[1];
+            $insert['name'] = $line[2];
+            $insert['department_id'] = $line[3];
+            
+// insert
+            DB::table('subjects')->insert($insert);
+
+            echo 'insert: ' . $line[2] ;
+            echo PHP_EOL;
+
+        }
+
+    
+
+    fclose($file_handle);
+
+    echo PHP_EOL;
+    echo '------------------------------------------ close file ------------------------------------------';
+    echo PHP_EOL;
+
     
     }
+    
 }
